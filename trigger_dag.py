2d10a5da10d884426992f8aade7eb8e952135343
Vishnu_B_G_Bharadwaja <146937608+vishnubgb-ca@users.noreply.github.com>
2024-12-24 19:03:41 +0530

    Update trigger_dag.py

--- trigger_dag.py
+++ trigger_dag.py
@@ -2,7 +2,7 @@ import paramiko
 import os
 import requests
 deployment_url = os.environ["airflow_url"]
-dag_id = "Supply-chain-back-order-prediction_Testtest"
+dag_id = "supplychain_VishnuBharadwaja"
 response = requests.post(
     url=f"{deployment_url}/api/v1/dags/{dag_id}/dagRuns",
     auth=(os.environ['airflow_username'], os.environ['airflow_password']),
